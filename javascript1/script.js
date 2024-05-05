61d937e8118df2cc92c79f551bedc76a2585aa8d
awaismalik0001 <malikmalikawaisali786910@gmail.com>
2024-05-05 09:30:43 -0700

    firstly upload

---
+++ javascript1/script.js
@@ -0,0 +1,114 @@
+// AGE CAlulate
+let birthyear=2004;
+let currenyear=2024;
+let age=2024-2004;
+console.log(age)
+
+// AREA OF CIRCLE (A=pie*r*r)
+let pie= 3.14;
+let r=3;
+let area=pie*r*r
+console.log(area)
+
+// AREA OF Rectangle (R=length*width)
+let length=5;
+let width=5;
+let Rectanglearea= length*width;
+console.log(Rectanglearea)
+
+// AREA OF cube(SA=6a^2)
+let ab=5;
+let areaofcube=ab*ab;
+let surfacearea=6*areaofcube;
+console.log(surfacearea)
+
+// FAHRENITE TO CELSIUS(f=c*9/5+32)
+let celsius=50;
+let Fahrenite=celsius*9/5+32;
+console.log(Fahrenite)
+
+// CELSIUS TO FAHRENITE (c=(f-32)*5/9)
+let fahrenitea=50;
+let celsiusa=(fahrenitea-32)*5/9
+console.log(celsiusa)
+
+// Convert second into minute
+let second=1000;
+let minute=second/60;
+console.log(minute)
+
+// Calaulate Percentage For Students (percentage=number/total number*100)
+let totalstudent=100;
+let male=64;
+let female=36;
+let malepercentage=male/totalstudent*100;
+let femalepercentage=female/totalstudent*100;
+console.log(malepercentage,femalepercentage)
+
+// program that converts given number of days in to weeks and days 
+let days=37;
+let oneweek=7;
+let totalweek=days/oneweek;
+console.log(totalweek)
+
+// increment and Decrement Operator:
+//  - Q1:
+ let c = 2;
+let d = ++c * 2; 
+console.log(c)
+console.log(d)
+// - Q2
+let x = 5;
+let y = x-- + 2;
+console.log(x)
+console.log(y)
+// - Q3 
+let xx = 3;
+let yy = ++xx + xx++ + ++xx;
+console.log(xx)
+console.log(yy)
+// - Q4 
+let m = 2;
+let n = ++m * m++ * --m;
+console.log(m)
+console.log(n)
+
+// - Q5
+let e= 3;
+let f = 5;
+let result = ++e + f-- - e++ + --f;
+console.log(e)
+console.log(f)
+// - Q6
+let rr = 2;
+let t = 4;
+let yyy = r++ + ++t - --r + t--;
+console.log(t)
+console.log(rr)
+console.log(yyy)
+
+// - Q7
+ let gg = 5;
+let hh = 3;
+let jj = 2;
+let kk = 7;
+
+let finalresult = ++gg * (hh-- + jj) / --kk;
+console.log(gg)
+console.log(hh)
+console.log(jj)
+console.log(kk)
+console.log(finalresult)
+
+// - Q8
+let z = 2;
+let zz= 3;
+let zzz = 4;
+let resultfinal = z++ * (--z + zz) / (zzz-- - z);
+console.log(z)
+console.log(zz)
+console.log(zzz)
+console.log(resultfinal)
+
+
+
